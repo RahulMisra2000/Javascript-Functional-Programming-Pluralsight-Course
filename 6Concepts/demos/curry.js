326f0c5079eacc269442fa6c40c2dc1c319140e5
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-15 19:44:49 -0400

    Comment

--- 6Concepts/demos/curry.js
+++ 6Concepts/demos/curry.js
@@ -2,13 +2,13 @@ const people = require('./people.json');
 
 // Returns a filtered array whose every element has prop equal to value
 // 3 input variables ------------
-const filterEq  = (prop, value, arr) => { return arr.filter((elem) => { return elem[prop] === value; }); };
+const filterEq  = (prop, value, arr)  => { return arr.filter((elem) => { return elem[prop] === value; }); };
 
 // 2 input variables ------------
-const isMarried = (value, arr) => filterEq('married', value, arr);
+const isMarried = (value, arr)        => { return filterEq('married', value, arr); }
 
 // 1 input variable ------------
-const married   = (arr)        => isMarried(true, arr);
+const married   = (arr)               => { return isMarried(true, arr); }
 
 // The above is called currying as we lower the number of input variables.
 
